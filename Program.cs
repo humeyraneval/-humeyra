37ea2dde79a4dfea11a43671a6151a8f81f7d83a
humeyraneval <115422775+humeyraneval@users.noreply.github.com>
2022-10-11 10:30:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace hata_ayıklama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* try
+            {
+              Console.WriteLine("Lütfen bir sayı giriniz.");
+              int a = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("Girdiğiniz sayı:" + a);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Yanlış bir ifade girdiniz.");
+                Console.WriteLine("Hata:" + ex);
+             
+            }*/
+            try
+            {
+                //int a = int.Parse(null);
+                int a = int.Parse("kırmızı");
+
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz.");
+                Console.WriteLine("hata:" + ex);
+            }   
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Yanlı fortmatta bir değer girdinzi.");
+                Console.WriteLine("hata:" + ex);
+
+            }
+        
+        }
+    }
+}
